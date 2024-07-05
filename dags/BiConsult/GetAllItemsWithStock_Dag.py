eebf44c142bb693c9440f43d464e80a6cd520c47
izida <109024816+yaizida@users.noreply.github.com>
2024-07-05 08:42:26 +0300

    Update GetAllItemsWithStock_Dag.py

--- dags/BiConsult/GetAllItemsWithStock_Dag.py
+++ dags/BiConsult/GetAllItemsWithStock_Dag.py
@@ -6,25 +6,32 @@ from datetime import datetime, timedelta
 
 import pendulum
 import pandas as pd
+import numpy as np
 from airflow.operators.python import get_current_context
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.smtp.hooks.smtp import SmtpHook
 from airflow.decorators import dag, task
 from airflow.models import Variable
 from suds.client import Client
 from airflow.decorators import dag, task
 
 
-POSTGRES_CONN_ID = ''
-TABLE_NAME = ''
-CUR_DIR = os.path.abspath(os.path.dirname(__file__))
+POSTGRES_CONN_ID = 'airflowdb'
+TABLE_NAME = 'ods.items'
 KEY = '1vSu3k1DvCE='
+PG_HOOK = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
+LAYER, NAME_DAG = os.path.realpath(__file__).split('\\')[-2:]
+
 client = Client('https://sales-ws.farfetch.com/pub/apistock.asmx?wsdl',
                 timeout=30)
+
 log = logging.getLogger('suds.client')
 log.setLevel(logging.WARNING)
 handler = logging.FileHandler('detail.log', 'a', 'utf-8')
 handler.setFormatter(logging.Formatter('%(asctime)s-%(levelname)s-%(message)s')) # noqa
 log.addHandler(handler)
+
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
@@ -36,7 +43,7 @@ default_args = {
 @dag(
     dag_id='GetAllItemsWithStock',
     default_args=default_args,
-    schedule='@once',
+    schedule='0 0 21  *   * ',
     catchup=False,
     concurrency=4,
     start_date=pendulum.yesterday("Europe/Moscow"),
@@ -47,144 +54,141 @@ default_args = {
 def farfetch_gettAll_withStock():
     # GetAllItemsWithStock
     @task
-    def get_all_items_wStock(**kwargs):
+    def get_all_items_wStock() -> pd.DataFrame:
         response = client.service.GetAllItemsWithStock(KEY)
         data = response.GetAllItemsWithStockResult.diffgram[0].DocumentElement[0].Table # noqa
         header = [x[0] for x in data[0]]
         body = [[str(j[1][0] if isinstance(j[1], list)
                  else str(j[1])) for j in row]for row in data]
-        df = pd.DataFrame(body, columns=header)
+        api_df = pd.DataFrame(body, columns=header)
+        api_df = api_df.drop(['_id', '_rowOrder'], axis=1)
+        logging.info(f'Дата фрейм get_all_items_wStock: {api_df.head()}')
+        logging.info(f'Длина датафрейма в get_all_items_wStock: {len(api_df)}')
+        
+        
+        api_df['inserted_dttm'] = ''
+        api_df['updated_dttm'] = ''
+        db_df = PG_HOOK.get_pandas_df(sql=f"SELECT * FROM {TABLE_NAME}")
+        api_df = api_df.rename(columns=dict(zip(api_df.columns.to_list(), db_df.columns.to_list())))
 
-        logging.info(f'Дата фрейм: {df.head()}')
-        return df
+        logging.info(api_df)
+        return api_df 
 
     @task
-    def fetch_db_data():
-        """Получает данные из PostgreSQL."""
-        pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
-        df = pg_hook.get_pandas_df(sql=f"SELECT * FROM {TABLE_NAME}")
-        return df
+    def update(api_df: pd.DataFrame):
+        import inspect
+        from notification_dt1 import EmailAlertMsg
+
+        db_df = PG_HOOK.get_pandas_df(sql=f"SELECT * FROM {TABLE_NAME}")
+        api_df['combined_key'] = api_df['id'].astype(str) + api_df['Size']
+        db_df['combined_key'] = db_df['id'].astype(str) + db_df['Size']
+        
+        today_date = pendulum.now().in_timezone('Europe/Moscow').format('YYYY-MM-DD HH:mm:ss')
+        api_df['inserted_dttm'] = today_date
+        for i in range(len(api_df)):
+            match_index = db_df[db_df['combined_key'] == api_df['combined_key'][i]].index
+            if len(match_index) > 0:
+                for idx in match_index:
+                    for col in api_df.columns:
+                        if col != 'inserted_dttm':  
+                            if api_df.iloc[i][col] != db_df.iloc[idx][col]:
+                                db_df.loc[idx, col] = api_df.iloc[i][col]
+                                if col == 'updated_dttm':
+                                    db_df.loc[idx, 'updated_dttm'] = today_date
+        
+        s_buf = io.StringIO()
+        try:
+            db_df = db_df.drop(columns='index')
+        except Exception:
+            logging.info('Index does not exist')
+
+        db_df.drop('combined_key', axis=1, inplace=True)
+        db_df = db_df.replace(np.nan, None)
+        db_df = db_df.replace([pd.NaT], [None])
+        db_df = db_df.replace('None', None)
+        db_df = db_df.rename(columns={'Group': '"Group"',
+                                      'Name': '"Name"', 
+                                      'Style': '"Style"',
+                                      'Size': '"Size"'
+                                      })
+        db_df.to_csv(s_buf, index=False)
+        
+        try:
+            with PG_HOOK.get_conn() as conn:
+                cur = conn.cursor()
+                cur.execute(f"TRUNCATE TABLE {TABLE_NAME}")
+                s_buf.seek(0)  # Сбрасываем указатель в буфере
+                logging.info("Inserting data to DWH")
+                query = f"""COPY {TABLE_NAME} ({', '.join(db_df.columns.to_list())}) FROM STDIN DELIMITER ',' CSV HEADER"""
+                logging.info("Copy query: %s", query)
+                cur.copy_expert(query, s_buf)
+                conn.commit()
+                logging.info('Updated Values is Done GetAllItemsWithStock')
+        except Exception as error:
+            conn.rollback()
+            error = error
+            function_name = inspect.stack()[1].function 
+            logging.error(f"{LAYER}.{NAME_DAG}.{function_name} Ошибка при копировании данных: {error}")
+          
+        EmailAlertMsg(LAYER, NAME_DAG, function_name,  error).send_email()
+
 
 
     @task
-    def compare_and_update_data(api_df: pd.DataFrame, db_df: pd.DataFrame):
-        """Сравнивает DataFrame и обновляет базу данных."""
+    def get_new(api_df: pd.DataFrame):
+        import inspect
+        from notification_dt1 import EmailAlertMsg
 
-        common_cols = ["ItemID"]
-        # Совместим df для сравнения
-        merged_df = api_df.merge(
-            db_df,
-            on=common_cols,
-            how="left",
-            suffixes=("_api", "_db"),
-        )
+        function_name = inspect.stack()[1].function 
 
-        # Новые записи: записи, которые есть в API, но отсутствуют в базе
-        new_records = merged_df[merged_df["ItemID_db"].isnull()]
-        if not new_records.empty:
-            logging.info(f"Найдены {len(new_records)} новых записей.")
-            # Добавьте столбцы для created и updated
-            new_records["created"] = datetime.now()
-            new_records["updated"] = datetime.now()
-            # Преобразование данных
-            new_records["item_id"] = pd.to_numeric(new_records["ItemID"])
-            new_records["quantity"] = pd.to_numeric(new_records["Quantity"])
-            new_records["gender_id"] = pd.to_numeric(new_records["GenderID"])
-            new_records["season_id"] = pd.to_numeric(new_records["SeasonID"])
-            # Выберите нужные столбцы для вставки
-            insert_cols = [
-                "item_id",
-                "brand",
-                "department",
-                "group_stocks",
-                "name_stocks",
-                "full_price",
-                "discount_price",
-                "size",
-                "quantity",
-                "size_range_type",
-                "style",
-                "gender_id",
-                "gender",
-                "season_id",
-                "season",
-                "barcode",
-                "sku",
-                "store_barcode",
-                "created",
-                "updated",
-            ]
-            # Вставьте новые записи в базу данных
-            pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
-            pg_hook.insert_rows(
-                table=TABLE_NAME, rows=new_records[insert_cols].values.tolist()
-            )
+        db_df = PG_HOOK.get_pandas_df(sql=f"SELECT * FROM {TABLE_NAME}")
+        api_df['combined_key'] = api_df['id'].astype(str) + api_df['Size']
+        db_df['combined_key'] = db_df['id'].astype(str) + db_df['Size']
+        missing_values = api_df[~api_df['combined_key'].isin(db_df['combined_key'])]
+        missing_values.drop('combined_key', axis=1, inplace=True)
+        missing_values = missing_values.replace(np.nan, None)
+        missing_values = missing_values.replace([pd.NaT], [None])
+        missing_values = missing_values.rename(columns={'Group': '"Group"',
+                                                        'Name': '"Name"', 
+                                                        'Style': '"Style"',
+                                                        'Size': '"Size"'
+                                                        })  
+        missing_values = missing_values.drop(['inserted_dttm', 'updated_dttm'], axis=1)
+        
+        if len(missing_values) > 0:
+            logging.info(f'Значений которых нет в БД: {missing_values}')
+            logging.info(f'Типы даных датафрейма: {db_df.dtypes}')
+            logging.info(f'Столбцы  DataFrame {missing_values.columns}')
+            s_buf = io.StringIO()
+            # missing_values = db_df.drop(columns='index')
+            missing_values.to_csv(s_buf, index=False)
+            print(db_df.dtypes)
+            s_buf.seek(0)
+            try:
+                with PG_HOOK.get_conn() as conn:
+                    cur = conn.cursor()
+                    logging.info("Inserting data to DWH")
+                    query = f"""COPY {TABLE_NAME} ({', '.join(missing_values.columns.to_list())}) FROM STDIN DELIMITER ',' CSV HEADER"""
+                    logging.info("Copy query: %s", query)
+                    cur.copy_expert(query, s_buf)
+                    conn.commit()
+            except Exception as error:
+                error = error
+                conn.rollback()
+                logging.error(f"{LAYER}.{NAME_DAG}.{function_name} Ошибка при копировании данных: {error}")
+        else:
+            logging.info('No new data to upload')
 
-        # Измененные записи: записи, которые есть в API и в базе, но значения отличаются
-        changed_records = merged_df[
-            (merged_df["ItemID_db"].notnull())
-            & (
-                (merged_df["Brand_api"] != merged_df["Brand_db"])
-                | (merged_df["Department_api"] != merged_df["Department_db"])
-                | (merged_df["Group_api"] != merged_df["Group_db"])
-                | (merged_df["Name_api"] != merged_df["Name_db"])
-                | (merged_df["FullPrice_api"] != merged_df["FullPrice_db"])
-                | (merged_df["DiscountPrice_api"] != merged_df["DiscountPrice_db"])
-                | (merged_df["Size_api"] != merged_df["Size_db"])
-                | (merged_df["Quantity_api"] != merged_df["Quantity_db"])
-                | (merged_df["SizeRangeType_api"] != merged_df["SizeRangeType_db"])
-                | (merged_df["Style_api"] != merged_df["Style_db"])
-                | (merged_df["GenderID_api"] != merged_df["GenderID_db"])
-                | (merged_df["Gender_api"] != merged_df["Gender_db"])
-                | (merged_df["SeasonID_api"] != merged_df["SeasonID_db"])
-                | (merged_df["Season_api"] != merged_df["Season_db"])
-                | (merged_df["Barcode_api"] != merged_df["Barcode_db"])
-                | (merged_df["SKU_api"] != merged_df["SKU_db"])
-                | (merged_df["StoreBarcode_api"] != merged_df["StoreBarcode_db"])
-            )
-        ]
-        if not changed_records.empty:
-            logging.info(f"Найдены {len(changed_records)} измененных записей.")
-            # Обновите записи в базе данных
-            pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
-            for index, row in changed_records.iterrows():
-                update_query = f"""
-                    UPDATE {TABLE_NAME}
-                    SET brand = %s, department = %s, group_stocks = %s, name_stocks = %s,
-                        full_price = %s, discount_price = %s, "size" = %s, quantity = %s,
-                        size_range_type = %s, "style" = %s, gender_id = %s, gender = %s,
-                        season_id = %s, season = %s, barcode = %s, sku = %s, store_barcode = %s,
-                        updated = %s
-                    WHERE item_id = %s;
-                """
-                pg_hook.run(
-                    update_query,
-                    (
-                        row["Brand_api"],
-                        row["Department_api"],
-                        row["Group_api"],
-                        row["Name_api"],
-                        row["FullPrice_api"],
-                        row["DiscountPrice_api"],
-                        row["Size_api"],
-                        row["Quantity_api"],
-                        row["SizeRangeType_api"],
-                        row["Style_api"],
-                        row["GenderID_api"],
-                        row["Gender_api"],
-                        row["SeasonID_api"],
-                        row["Season_api"],
-                        row["Barcode_api"],
-                        row["SKU_api"],
-                        row["StoreBarcode_api"],
-                        datetime.now(),
-                        row["ItemID_api"],
-                    ),
-                )
+        EmailAlertMsg(LAYER, NAME_DAG, function_name, error).send_email()
+        
 
-    api_data = get_all_items_wStock()
-    db_data = fetch_db_data()
-    compare_and_update_data(api_data, db_data)
 
+    data = get_all_items_wStock()
+    update = update(data)
+    get_new = get_new(data)
+
+    (
+        data >> update >> get_new
+    )
 
 farfetch_gettAll_withStock()
